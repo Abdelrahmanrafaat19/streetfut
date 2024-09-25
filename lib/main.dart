eea51650c50229044a4af4ec2708f07507a4e895
Abdelrahman Raafat Shoaib <93776528+Abdelrahmanrafaat19@users.noreply.github.com>
2024-09-25 17:01:50 +0300

    this is put splash screen in main

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:fut/core/storage.dart';
 import 'package:fut/feature/home/presentation/views/home_screen.dart';
 import 'package:fut/feature/layout/translation/localization_cubit.dart';
 import 'package:fut/feature/layout/translation/localization_state.dart';
+import 'package:fut/feature/splash_screen/splash_screen.dart';
 import 'package:fut/generated/l10n.dart';
 
 Future<void> main() async {
@@ -61,7 +62,7 @@ class MyApp extends StatelessWidget {
           ),
         );
       },
-      child: const HomeScreen(),
+      child: const SplashScreen(),
     );
   }
 }
